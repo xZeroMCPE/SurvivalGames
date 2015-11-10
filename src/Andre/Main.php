777f276ec8c11eb8283ae64eec0ce22459fdc55d
Andre <andrethegamer911@gmail.com>
2015-11-10 15:21:40 -0800

    Added Main.php

---
+++ src/Andre/Main.php
@@ -0,0 +1,13 @@
+
+####
+# SurvivalGames Made By: Andre_The_Gamer
+# basic things: 5%
+# Join Sign: 0%
+# Chest Things: 0%
+####
+
+namespace Andre;
+
+# THINGS PLUGIN WILL USE
+
+use pocketmine/server; // Isn't done
