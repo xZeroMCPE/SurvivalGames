d6a5a39ff71efcc7929e2896fe6ea0d0159111c5
Andre <andrethegamer911@gmail.com>
2015-11-23 14:48:29 -0500

    Added timer

--- src/Andre/Timer.php
+++ src/Andre/Timer.php
@@ -12,8 +12,11 @@ class Timer extends PluginTask{
   }
 
   public function onRun($tick){
-  
-  
+    
+    if ($tick = 0) {
+$this->getServer()->broadcastMessage("- Match will start in 1:150 Min !");
+} else {
+return false;
   }
 
 }
